2a8a450281ad15c0b14f0a725454ad74707c0ba1
Sonja Perković <soperkov@vsite.hr>
2024-09-17 14:01:19 +0200

    added login, models and services, database

--- Program.cs
+++ Program.cs
@@ -1,10 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using WebTarotReadings;
 using WebTarotReadings.Components;
+using WebTarotReadings.Models;
+using WebTarotReadings.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
 	.AddInteractiveServerComponents();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("TarotContext") ??
+           throw new InvalidOperationException("Connection string ParkingContext not found")));
+
+builder.Services.AddScoped<HoroscopeSignService>();
+builder.Services.AddScoped<TarotCardsService>();
+builder.Services.AddScoped<UserService>();
+builder.Services.AddSingleton<LoggedInUserModel>();
+
+builder.Services.AddBlazorBootstrap();
+
 
 var app = builder.Build();
 
--- Services/TarotCardsService.cs
+++ Services/TarotCardsService.cs
@@ -1,6 +1,18 @@
-﻿namespace WebTarotReadings.Services
+﻿using WebTarotReadings.Models;
+
+namespace WebTarotReadings.Services
 {
 	public class TarotCardsService
 	{
+		public readonly AppDbContext _context;
+		public TarotCardsService(AppDbContext context)
+		{
+			_context = context;
+		}
+
+		public TarotCardsModel GetTarotCard(int cardId)
+		{
+			return _context.TarotCards.FirstOrDefault(s => s.Id == cardId);
+		}
 	}
 }
